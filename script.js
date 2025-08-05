c5bb83d9073ccf89fbc5898deb8167e8ab6217b7
Jhone-cmd <jhone.1941@gmail.com>
2025-08-04 21:18:00 -0300

    chore: performing the submit button logic, and structuring the form

--- script.js
+++ script.js
@@ -7,7 +7,29 @@ const form = document.getElementById('form')
 
 const enviarFormulario = (event) => {
     event.preventDefault()
-    console.log(event)
+    const apiKey = apiKeyInput.value
+    const game = gameSelect.value
+    const question = questionInput.value
+
+    if (!apiKey || !game || !question) {
+        alert('Por favor, preencha todos os campos.')
+        return
+    }
+
+    askButton.disabled = true
+    askButton.textContent = 'Enviando pergunta...'
+    askButton.classList.add('loading')
+
+    try {
+
+    } catch (error) {
+        console.log('Erro ao enviar o formulário:', error);
+
+    } finally {
+        askButton.disabled = false
+        askButton.textContent = 'Pergunta'
+        askButton.classList.remove('loading')
+    }
 }
 
 form.addEventListener('submit', enviarFormulario)
