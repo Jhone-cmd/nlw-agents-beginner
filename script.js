502095891c3d44b7602a8c3c5ab85c7ed265fa49
Jhone-cmd <jhone.1941@gmail.com>
2025-08-06 10:11:43 -0300

    chore: improving flame for gemini by integrating the tools and making a better prompt for the question

--- script.js
+++ script.js
@@ -13,15 +13,45 @@ function markdownToHTML(text) {
 async function questionAI(question, game, apiKey) {
     const model = "gemini-2.5-flash"
     const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
-    const text = `Responda a seguinte pergunta sobre o jogo ${game} de forma clara e objetiva: ${question}`
+    const text = `
+    ## Especialidade
+    Você é um especialista assistente de meta para o jogo ${game}
+
+    ## Tarefa
+    Você deve responder as perguntas do usuário com base no seu conhecimento do jogo, estratégias, build e dicas
+
+    ## Regras
+    - Se você não sabe a resposta, responda com 'Não sei' e não tente inventar uma resposta.
+    - Se a pergunta não está relacionada ao jogo, responda com 'Essa pergunta não está relacionada ao jogo'
+    - Considere a data atual ${new Date().toLocaleDateString()}
+    - Faça pesquisas atualizadas sobre o patch atual, baseado na data atual, para dar uma resposta coerente.
+    - Nunca responda itens que vc não tenha certeza de que existe no patch atual.
+
+    ## Resposta
+    - Economize na resposta, seja direto e responda no máximo 500 caracteres
+    - Responda em markdown
+    - Não precisa fazer nenhuma saudação ou despedida, apenas responda o que o usuário está querendo.
+
+    ## Exemplo de resposta
+    pergunta do usuário: Melhor build rengar jungle
+    resposta: A build mais atual é: \n\n **Itens:**\n\n coloque os itens aqui.\n\n**Runas:**\n\nexemplo de runas\n\n
+
+    ---
+    Aqui está a pergunta do usuário: ${question}
+  `
 
     const payload = {
         contents: [{
+            role: UserActivation,
             parts: [{
                 text
             }]
         }]
     }
+
+    const tools = [{
+        google_search: {}
+    }]
 
 
 
@@ -31,7 +61,7 @@ async function questionAI(question, game, apiKey) {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(payload)
+        body: JSON.stringify(payload, { tools })
     })
 
     const data = await response.json()
